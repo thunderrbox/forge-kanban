24e61068e23143f7c521b0f77d4c49119ddcfcd5
thunderrbox <168538688+thunderrbox@users.noreply.github.com>
2026-07-17 10:24:12 +0530

    fix: Define list_id foreign key in CardList cards relationship

--- backend/app/Models/CardList.php
+++ backend/app/Models/CardList.php
@@ -7,6 +7,6 @@ class CardList extends Model {
         return $this->belongsTo(Board::class);
     }
     public function cards() {
-        return $this->hasMany(Card::class)->orderBy('position');
+        return $this->hasMany(Card::class, 'list_id')->orderBy('position');
     }
 }
